97642b322d168a0af3b596e6caebf63be42d42c1
Robin <lassak.robin@gmail.com>
2025-06-03 12:32:48 +0200

    Edit department enity 2

--- Migrations/20250603103020_upravaDepartment2.Designer.cs
+++ Migrations/20250603103020_upravaDepartment2.Designer.cs
@@ -3,6 +3,7 @@ using AttenanceSystemApp;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AttenanceSystemApp.Migrations
 {
     [DbContext(typeof(AttenanceDbContext))]
-    partial class AttenanceDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250603103020_upravaDepartment2")]
+    partial class upravaDepartment2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
