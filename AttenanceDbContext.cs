0ef059f2cc0ff04de56c47afcb865eb1d98e48c3
Robin <lassak.robin@gmail.com>
2025-06-03 10:00:04 +0200

    First commit

---
+++ AttenanceDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AttenanceSystemApp
+{
+    public class AttenanceDbContext : DbContext
+    {
+        public AttenanceDbContext(DbContextOptions<AttenanceDbContext> options)
+            :base(options) { }
+    }
+}
---
+++ Models/Department.cs
@@ -0,0 +1,8 @@
+﻿namespace AttenanceSystemApp.Models
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+    }
+}
