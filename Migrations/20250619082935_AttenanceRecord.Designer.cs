3180994310064fead97b72ec46a3ff32eb02a1c8
Robin <lassak.robin@gmail.com>
2025-06-19 10:32:02 +0200

    AttenanceRecord migration

--- Migrations/20250619082935_AttenanceRecord.Designer.cs
+++ Migrations/20250619082935_AttenanceRecord.Designer.cs
@@ -4,6 +4,7 @@ using AttenanceSystemApp;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AttenanceSystemApp.Migrations
 {
     [DbContext(typeof(AttenanceDbContext))]
-    partial class AttenanceDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250619082935_AttenanceRecord")]
+    partial class AttenanceRecord
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20250619082935_AttenanceRecord.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AttenanceSystemApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class AttenanceRecord : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AttenanceRecords",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    AttenanceIn = table.Column<TimeSpan>(type: "time", nullable: true),
+                    AttenanceOut = table.Column<TimeSpan>(type: "time", nullable: true),
+                    DoctorIn = table.Column<TimeSpan>(type: "time", nullable: true),
+                    DoctorOut = table.Column<TimeSpan>(type: "time", nullable: true),
+                    SmokeIn = table.Column<TimeSpan>(type: "time", nullable: true),
+                    SmokeOut = table.Column<TimeSpan>(type: "time", nullable: true),
+                    IsVacation = table.Column<bool>(type: "bit", nullable: false),
+                    IsSickLeave = table.Column<bool>(type: "bit", nullable: false),
+                    WorkedHours = table.Column<TimeSpan>(type: "time", nullable: true),
+                    EmployeeId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AttenanceRecords", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_AttenanceRecords_Employees_EmployeeId",
+                        column: x => x.EmployeeId,
+                        principalTable: "Employees",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AttenanceRecords_EmployeeId",
+                table: "AttenanceRecords",
+                column: "EmployeeId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AttenanceRecords");
+        }
+    }
+}
