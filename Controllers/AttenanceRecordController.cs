7065253250dfde03f2258711f7cb4958ff5a0b07
Robin <lassak.robin@gmail.com>
2025-06-25 11:07:19 +0200

    AttenanceRecordController with service

--- Controllers/AttenanceRecordController.cs
+++ Controllers/AttenanceRecordController.cs
@@ -1,12 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AttenanceSystemApp.Services;
+using Microsoft.AspNetCore.Mvc;
 
 namespace AttenanceSystemApp.Controllers
 {
     public class AttenanceRecordController : Controller
     {
+        AttenanceRecordService _attenanceRecordService;
+        public AttenanceRecordController(AttenanceRecordService attenanceRecordService)
+        {
+            _attenanceRecordService = attenanceRecordService;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
+        
     }
 }
