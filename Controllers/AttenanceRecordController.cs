309697370fa5be7d693b8e42f4deac61ee4b83b1
Robin <lassak.robin@gmail.com>
2025-06-25 10:39:09 +0200

    AttenanceRecord Controller+DTO

---
+++ Controllers/AttenanceRecordController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AttenanceSystemApp.Controllers
+{
+    public class AttenanceRecordController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ DTO/AttenanceRecordDTO.cs
@@ -0,0 +1,19 @@
+﻿namespace AttenanceSystemApp.DTO
+{
+    public class AttenanceRecordDTO
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public TimeSpan? AttenanceIn { get; set; }
+        public TimeSpan? AttenanceOut { get; set; }
+        public TimeSpan? DoctorIn { get; set; }
+        public TimeSpan? DoctorOut { get; set; }
+        public TimeSpan? SmokeIn { get; set; }
+        public TimeSpan? SmokeOut { get; set; }
+        public bool IsVacation { get; set; }
+        public bool IsSickLeave { get; set; }
+        public TimeSpan? WorkedHours { get; set; }
+        public int EmployeeId { get; set; }
+        public EmployeeDTO Employee { get; set; }
+    }
+}
