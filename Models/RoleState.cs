d388c04cc0ce9496f01275feee55e6e1ee2da50e
Robin <lassak.robin@gmail.com>
2025-06-26 18:38:53 +0200

    Role Models

---
+++ Models/RoleState.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace AttenanceSystemApp.Models
+{
+    public class RoleState
+    {
+        public IdentityRole Role { get; set; }
+        public IEnumerable<AppUser> Members { get; set; }
+        public IEnumerable<AppUser> NonMembers { get; set; }
+    }
+}
