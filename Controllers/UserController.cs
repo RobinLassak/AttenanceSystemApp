8efd769c335e0c296cb96b977b43fd4ccc949bba
Robin <lassak.robin@gmail.com>
2025-06-26 17:40:14 +0200

    User model+controller

---
+++ Controllers/UserController.cs
@@ -0,0 +1,24 @@
+﻿using AttenanceSystemApp.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AttenanceSystemApp.Controllers
+{
+    public class UserController : Controller
+    {
+        UserManager<AppUser> _userManager;
+        IPasswordHasher<AppUser> _passwordHasher;
+        IPasswordValidator<AppUser> _passwordValidator;
+        public UserController(UserManager<AppUser> userManager, IPasswordHasher<AppUser> passwordHasher,
+            IPasswordValidator<AppUser> passwordValidator)
+        {
+            _userManager = userManager;
+            _passwordHasher = passwordHasher;
+            _passwordValidator = passwordValidator;
+        }
+        public IActionResult Index()
+        {
+            return View(_userManager.Users);
+        }
+    }
+}
---
+++ ViewModels/UserViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AttenanceSystemApp.ViewModels
+{
+    public class UserViewModel
+    {
+        public string Name { get; set; }
+        [EmailAddress]
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
