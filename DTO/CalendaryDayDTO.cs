193035874ae153a6da65d47ec7560f5619339e2b
Robin <lassak.robin@gmail.com>
2025-06-26 10:16:58 +0200

    CalendarDay DTO

---
+++ DTO/CalendaryDayDTO.cs
@@ -0,0 +1,24 @@
+﻿namespace AttenanceSystemApp.DTO
+{
+    public class CalendaryDayDTO
+    {
+        public DateTime Date { get; set; }
+        public string DayName { get; set; }
+        public string DayType { get; set; }
+        public string? HolidayName { get; set; }
+
+        public string? AttenanceIn { get; set; }
+        public string? AttenanceOut { get; set; }
+
+        public string? DoctorIn { get; set; }
+        public string? DoctorOut { get; set; }
+
+        public string? SmokeIn { get; set; }
+        public string? SmokeOut { get; set; }
+
+        public bool IsVacation { get; set; }
+        public bool IsSickLeave { get; set; }
+
+        public string? WorkedHours { get; set; }
+    }
+}
