ef8601b61f216c07a17512debeba1ba8d99aa030
Robin <lassak.robin@gmail.com>
2025-06-26 18:43:28 +0200

    role controller

---
+++ Controllers/RoleController.cs
@@ -0,0 +1,135 @@
+﻿using AttenanceSystemApp.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AttenanceSystemApp.Controllers
+{
+    public class RoleController : Controller
+    {
+        RoleManager<IdentityRole> _roleManager;
+        UserManager<AppUser> _userManager;
+        public RoleController(RoleManager<IdentityRole> roleManager, UserManager<AppUser> userManager)
+        {
+            _roleManager = roleManager;
+            _userManager = userManager;
+        }
+        public IActionResult Index()
+        {
+            return View(_roleManager.Roles.OrderBy(role => role.Name));
+        }
+        //Vytvareni nove role
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(string name)
+        {
+            if (ModelState.IsValid)
+            {
+                IdentityResult result = await _roleManager.CreateAsync(new IdentityRole(name));
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    AddIdentityErrors(result);
+                }
+            }
+            return View(name);
+        }
+        //Mazani role
+        [HttpPost]
+        public async Task<IActionResult> Delete(string id)
+        {
+            IdentityRole roleToDelete = await _roleManager.FindByIdAsync(id);
+            if (roleToDelete != null)
+            {
+                var result = await _roleManager.DeleteAsync(roleToDelete);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    AddIdentityErrors(result);
+                }
+            }
+            ModelState.AddModelError("", "Role not found");
+            return View("Index", _roleManager.Roles);
+        }
+        public async Task<IActionResult> GetToDelete(string id)
+        {
+            var roleDetails = await _roleManager.FindByIdAsync(id);
+
+            if (roleDetails == null)
+            {
+                return NotFound();
+            }
+
+            return View(roleDetails);
+        }
+        //Editace role
+        [HttpGet]
+        public async Task<IActionResult> Edit(string id)
+        {
+            IdentityRole roleToEdit = await _roleManager.FindByIdAsync(id);
+            List<AppUser> members = new List<AppUser>();
+            List<AppUser> nonMembers = new List<AppUser>();
+            foreach (var user in _userManager.Users)
+            {
+                var list = await _userManager.IsInRoleAsync(user, roleToEdit.Name) ? members : nonMembers;
+                list.Add(user);
+            }
+            return View(new RoleState
+            {
+                Members = members,
+                NonMembers = nonMembers,
+                Role = roleToEdit,
+            });
+        }
+        public async Task<IActionResult> EditAsync(RoleModification roleModification)
+        {
+            if (ModelState.IsValid)
+            {
+                foreach (string userId in roleModification.AddIds ?? new string[] { })
+                {
+                    AppUser userToAdd = await _userManager.FindByIdAsync(userId);
+                    if (userToAdd != null)
+                    {
+                        IdentityResult result = await _userManager.AddToRoleAsync(userToAdd, roleModification.RoleName);
+                        if (!result.Succeeded)
+                        {
+                            AddIdentityErrors(result);
+                        }
+                    }
+                }
+                foreach (string userId in roleModification.DeleteIds ?? new string[] { })
+                {
+                    AppUser userToDelete = await _userManager.FindByIdAsync(userId);
+                    if (userToDelete != null)
+                    {
+                        IdentityResult result = await _userManager.RemoveFromRoleAsync(userToDelete, roleModification.RoleName);
+                        if (!result.Succeeded)
+                        {
+                            AddIdentityErrors(result);
+                        }
+                    }
+                }
+                return RedirectToAction("Index");
+            }
+            ModelState.AddModelError("", "Spatne zadana zmena, zkontroluj udaje");
+            return RedirectToAction("Index");
+        }
+        //Pomocne metody
+        private void AddIdentityErrors(IdentityResult result)
+        {
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError("", error.Description);
+            }
+        }
+    }
+}
