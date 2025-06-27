cc57f943ac9b9811342ab2eebdfbb1d6645b0eae
Robin <lassak.robin@gmail.com>
2025-06-27 08:44:32 +0200

    login view+viewModel

---
+++ ViewModels/LoginViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AttenanceSystemApp.ViewModels
+{
+    public class LoginViewModel
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+
+        [Display(Name = "Return URL")]
+        public string? ReturnURL { get; set; }
+        public bool Remember { get; set; }
+    }
+}
